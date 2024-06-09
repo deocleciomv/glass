4b8adc11d9740cf20b8baa92b442e47eb8d3d9db
deocleciomv <deocleciomiguelvolkers@outlook.com>
2024-06-09 11:30:46 -0300

    Add files via upload

---
+++ scripts/collapsible-questions.js
@@ -0,0 +1,15 @@
+/* Collapsible */
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("ativado");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
